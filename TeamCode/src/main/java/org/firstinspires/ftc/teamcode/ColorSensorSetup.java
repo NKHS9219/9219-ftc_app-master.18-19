030c56978905beed574f262ac957af16ba23a264
Devin Hunsberger <devin_hunsberger@students.nksd.net>
2018-12-04 21:12:53 -0500

    Disabled ColorSensorSetup (may not use it later, might as well disable it to reduce confusion). Added TensorFlow custom code vision file (contains MasterVision, SampleRandomizedPositions, TFLite, VisionJavaExample).
    Signed-off-by: Tomrish <thomas_rishworth@students.nksd.net>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorSetup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorSetup.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import android.graphics.Color;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -11,6 +12,7 @@ import com.qualcomm.robotcore.hardware.ColorSensor;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 @TeleOp(name="Color Hue Test")
+@Disabled
 public abstract class ColorSensorSetup extends LinearOpMode
 {
     //Declaring the color sensor

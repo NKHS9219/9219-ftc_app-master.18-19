5608d3815353569605a3a6f533cad35304878dee
Devin Hunsberger <devin_hunsberger@students.nksd.net>
2019-01-19 10:32:53 -0500

    Removed arbitrary programs for old tank tread system, edited Autonomous to include TFLite mineral sampling code
    Signed-off-by: Tomrish <thomas_rishworth@students.nksd.net>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp1ControllerStandardDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp1ControllerStandardDrive.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
@@ -11,25 +12,33 @@ public class TeleOp1ControllerStandardDrive extends LinearOpMode
 {
 
     //Declaring Motors
-    public DcMotor LeftTrack;
-    public DcMotor RightTrack;
+    public DcMotor LF;
+    public DcMotor RF;
+    public DcMotor LB;
+    public DcMotor RB;
+
 
     public void runOpMode() throws InterruptedException
     {
 
         //Initializing Motors
-        LeftTrack = hardwareMap.dcMotor.get("LeftTrack");
-        RightTrack = hardwareMap.dcMotor.get("RightTrack");
+        LF = hardwareMap.dcMotor.get("LeftFront");
+        RF = hardwareMap.dcMotor.get("RightFront");
+        LB = hardwareMap.dcMotor.get("LeftBack");
+        RB = hardwareMap.dcMotor.get("Rightback");
 
         //Reversing Motors
-        RightTrack.setDirection(DcMotor.Direction.REVERSE);
+        RF.setDirection(DcMotor.Direction.REVERSE);
+        RB.setDirection(DcMotor.Direction.REVERSE);
 
         waitForStart();
 
         while (opModeIsActive())
         {
-            LeftTrack.setPower(gamepad1.left_stick_y-gamepad1.right_stick_x);
-            RightTrack.setPower(gamepad1.left_stick_y+gamepad1.right_stick_x);
+            LF.setPower(gamepad1.left_stick_y-gamepad1.right_stick_x);
+            LB.setPower(gamepad1.left_stick_y-gamepad1.right_stick_x);
+            RF.setPower(gamepad1.left_stick_y+gamepad1.right_stick_x);
+            RB.setPower(gamepad1.left_stick_y+gamepad1.right_stick_x);
 
             idle();
         }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/Autonomous9219.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/Autonomous9219.java
@@ -1,25 +1,23 @@
 package org.firstinspires.ftc.teamcode.vision;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.vuforia.Vuforia;
 
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 
 @Autonomous(name = "Autonomous9219")
-@Disabled
 public class Autonomous9219 extends LinearOpMode {
     MasterVision vision;
     SampleRandomizedPositions goldPosition;
 
     //Declaring Motors
-    public DcMotor LeftTrack;
-    public DcMotor RightTrack;
+    public DcMotor LF;
+    public DcMotor RF;
+    public DcMotor LB;
+    public DcMotor RB;
+    public DcMotor Lift;
 
     //Declaring Servos
     public Servo Hook;
@@ -31,16 +29,21 @@ public class Autonomous9219 extends LinearOpMode {
         parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;// recommended camera direction
         parameters.vuforiaLicenseKey = "AXudchL/////AAABmaroK78zg0ZFpS1V9FRQ779Ts9dWY6EpHQGLt6dKWb8ocrUgdPeJ+/Ps1eVUW08uXKxF9Vt5LjTBuGHK1Pr7SN9yoDYZ86up4KUkiIEFpBInbKzipptJvUNXHEVA6trUzg3QDZBSctZPE4dJkK7PV6F4S2idWsWzMoyDP4SDIg6Tgo7JcK3HadqRIofDjjeRB0tM5IWhOE1DhPZ4U+OR5Y82mfnPqAYVLvX3EnhPejOsR8Q3Z9a7BQVJIdZtdZCEeWkaLb1jUYROvngXqYOTnQ4oUYtmPydrVCbLQgBMXaemwx64/A2pEGSIk4IPB86p/mLEISN/+1iDcpsz0NwYGF2knXWZvSD1KQSWPNO4ALv6\n";
 
-        vision = new MasterVision(parameters, hardwareMap, true, MasterVision.TFLiteAlgorithm.INFER_NONE);
+        vision = new MasterVision(parameters, hardwareMap, false, MasterVision.TFLiteAlgorithm.INFER_NONE);
         vision.init();// enables the camera overlay. this will take a couple of seconds
         vision.enable();// enables the tracking algorithms. this might also take a little time
 
         //Initializing Motors
-        LeftTrack = hardwareMap.dcMotor.get("LeftTrack");
-        RightTrack = hardwareMap.dcMotor.get("RightTrack");
+        LF = hardwareMap.dcMotor.get("LeftFront");
+        LB = hardwareMap.dcMotor.get("LeftBack");
+        RF = hardwareMap.dcMotor.get("RightFront");
+        RB = hardwareMap.dcMotor.get("RightBack");
+        Lift = hardwareMap.dcMotor.get("Arm");
 
         //Reversing Motor/Track
-        LeftTrack.setDirection(DcMotor.Direction.REVERSE);
+        LF.setDirection(DcMotor.Direction.REVERSE);
+        LB.setDirection(DcMotor.Direction.REVERSE);
+        Lift.setDirection(DcMotor.Direction.REVERSE);
 
         //Initializing Servos
         Hook = hardwareMap.servo.get("Hook");
@@ -59,76 +62,121 @@ public class Autonomous9219 extends LinearOpMode {
 
         telemetry.addData("goldPosition was", goldPosition);// giving feedback
 
+
+        LiftRaise(1, 1000);
+        Hook.setPosition(1);
+        LiftLower(1, 1000);
+
+        //phone must be horizontal and the camera on the left side.
         switch (goldPosition) { // using for things in the autonomous program
             case LEFT:
                 telemetry.addLine("going to the left");
-                TankRight(1,150);
+                TankLeft(1, 500);
+                TankForward(1, 1500);
                 break;
 
             case CENTER:
                 telemetry.addLine("going straight");
-                TankForward(1,100);
+                TankForward(1,2500);
                 break;
 
             case RIGHT:
                 telemetry.addLine("going to the right");
-                TankLeft(1,150);
+                TankRight(1,500);
+                TankForward(1,1500);
                 break;
 
-            default:
-                telemetry.addLine("staying put");
+            case UNKNOWN:
+                telemetry.addLine("not moving");
+                Thread.sleep(5000);
+            break;
         }
+
         telemetry.update();
         vision.shutdown();
     }
 
-    public void TankForward(double power, int target) {
+    public void LiftRaise(double power, int target)
+    {
         ResetEncoders();
-        LeftTrack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        RightTrack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        Lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        Lift.setPower(power);
+        Lift.setTargetPosition(target);
+    }
 
-        LeftTrack.setTargetPosition(target);
+    public void LiftLower(double power, int target)
+    {
+        ResetEncoders();
+        Lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        Lift.setPower(power);
+        Lift.setTargetPosition(target);
+    }
 
-        while (LeftTrack.isBusy()) {
-            LeftTrack.setPower(power);
-            RightTrack.setPower(power);
+    public void TankForward(double power, int target)
+    {
+        ResetEncoders();
+        LF.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        LB.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        RF.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        RB.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        LF.setTargetPosition(target);
+
+
+        while (LF.isBusy()) {
+            LF.setPower(power);
+            LB.setPower(power);
+            RF.setPower(power);
+            RB.setPower(power);
         }
 
-        LeftTrack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        RightTrack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        LF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        LB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        RF.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        RB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
     public void ResetEncoders()
     {
-        LeftTrack.setMode((DcMotor.RunMode.STOP_AND_RESET_ENCODER));
-        RightTrack.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        LF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        LB.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        RF.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        RB.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
     }
 
     public void TankBackward(double power,int target)
     {
         ResetEncoders();
-        LeftTrack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        RightTrack.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        LF.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        LB.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        RF.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        RB.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        LeftTrack.setTargetPosition(target);
+        LF.setTargetPosition(target);
 
-        while(LeftTrack.isBusy()) {
-            LeftTrack.setPower(-power);
-            RightTrack.setPower(-power);
+        while(LF.isBusy()) {
+            LF.setPower(-power);
+            LB.setPower(-power);
+            RF.setPower(-power);
+            RB.setPower(-power);
         }
     }
 
     public void TankLeft(double power,int time) throws InterruptedException
     {
-        LeftTrack.setPower(-power);
-        RightTrack.setPower(power);
+        LF.setPower(-power);
+        LB.setPower(-power);
+        RF.setPower(power);
+        RF.setPower(power);
         Thread.sleep(time);
     }
 
     public void TankRight(double power,int time) throws InterruptedException
     {
-        LeftTrack.setPower(power);
-        RightTrack.setPower(-power);
+        LF.setPower(power);
+        LB.setPower(power);
+        RF.setPower(-power);
+        RB.setPower(-power);
         Thread.sleep(time);
     }
 }
